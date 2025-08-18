5f996c3075bc31e41fd994c613aace1e9ba7fd2b
hypermetropia <sanzidul.islam155@gmail.com>
2025-08-19 01:42:36 +0600

    Designed Login Panel

---
+++ MainFrame.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import panels.*;
+
+public class MainFrame extends JFrame {
+    public static void main(String[] args) {
+        CardLayout cardLayout074 = new CardLayout();
+
+        JPanel cardPanel074 = new JPanel(cardLayout074);
+
+        // add panels
+        cardPanel074.add(new LoginPanel(cardLayout074, cardPanel074), "login");
+        cardPanel074.add(new RegisterPanel(cardLayout074, cardPanel074), "register");
+        cardPanel074.setPreferredSize(new Dimension(500,600));
+        // main panel
+        JPanel mainPanel074 = new JPanel();
+        mainPanel074.setLayout(new GridBagLayout());
+        mainPanel074.setBackground(new Color(199, 196, 191));
+        mainPanel074.add(cardPanel074);
+        //
+        MainFrame mainFrame074 = new MainFrame();
+        mainFrame074.add(mainPanel074);
+        mainFrame074.setVisible(true);
+        mainFrame074.setSize(1200, 900);
+        mainFrame074.setTitle("Shohoj Travels");
+    }
+
+}
--- panels/LoginPanel.java
+++ panels/LoginPanel.java
@@ -1,22 +1,99 @@
 package panels;
+
 import java.util.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-public class LoginPanel extends JPanel{
-    JLabel login074 = new JLabel();
-    JButton registerButton074 = new JButton();
-    public LoginPanel(CardLayout card, JPanel mainPanel){
-        login074.setText("Login");
-        login074.setBounds(10, 10, 10, 30);
-        login074.setFont( new Font(Font.SANS_SERIF, Font.PLAIN, 20));
-        add(login074);
-        
-        registerButton074.setBounds(100,100,100,100);
-        registerButton074.setText("Register");
-        registerButton074.addActionListener(e -> card.show(mainPanel, "register"));
-        add(registerButton074);
-        
+
+public class LoginPanel extends JPanel {
+    JLabel loginLabel074 = new JLabel();
+    JLabel usernameLabel074 = new JLabel();
+    JLabel passLabel074 = new JLabel();
+    JPasswordField passField = new JPasswordField();
+
+    JTextField loginField = new JTextField();
+    JButton toRegisterButton074 = new JButton();
+    JButton loginButton074 = new JButton();
+
+    public LoginPanel(CardLayout card, JPanel mainPanel) {
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        setBorder(new EmptyBorder(80, 50, 20, 50));
+        setBackground(Color.WHITE);
+
+        // Login title
+        loginLabel074.setText("Login");
+        loginLabel074.setForeground(new Color(65, 119, 135));
+        loginLabel074.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 40));
+        loginLabel074.setAlignmentX(CENTER_ALIGNMENT);
+        add(loginLabel074);
+
+        add(Box.createRigidArea(new Dimension(0, 80)));
+
+        // username panel with BoxLayout
+        JPanel usernamePanel = new JPanel();
+        usernamePanel.setLayout(new BoxLayout(usernamePanel, BoxLayout.X_AXIS));
+        usernamePanel.setBackground(Color.WHITE);
+        // Label
+        usernameLabel074.setText("Username:");
+        usernameLabel074.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 26));
+        usernamePanel.add(usernameLabel074);
+
+        // Small spacing between label and text field
+        usernamePanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        // TextField
+        loginField.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 23));
+        loginField.setMaximumSize(new Dimension(500, 35));
+        usernamePanel.add(loginField);
+        // Add to main panel
+        add(usernamePanel);
+        // Pass
+        // pass panel with BoxLayout
+        add(Box.createRigidArea(new Dimension(0, 20)));
+        JPanel passPanel074 = new JPanel();
+        passPanel074.setLayout(new BoxLayout(passPanel074, BoxLayout.X_AXIS));
+        passPanel074.setBackground(Color.WHITE);
+
+        // Label
+        passLabel074.setText("Password:");
+        passLabel074.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 26));
+        passPanel074.add(passLabel074);
+
+        // Small spacing between label and text field
+        passPanel074.add(Box.createRigidArea(new Dimension(13, 0)));
+        // PassField
+        passField.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 23));
+        passField.setMaximumSize(new Dimension(500, 35));
+        passPanel074.add(passField);
+        add(passPanel074);
+
+        // login Button
+        add(Box.createRigidArea(new Dimension(0, 40)));
+        loginButton074.setFont(new Font(Font.SANS_SERIF, Font.BOLD,25));
+        loginButton074.setText("Login");
+        loginButton074.setForeground(Color.WHITE);
+        loginButton074.setBackground(Color.decode("#6200EE"));
+        loginButton074.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
+        loginButton074.setAlignmentX(CENTER_ALIGNMENT);
+        loginButton074.setFocusPainted(false);
+        add(loginButton074);
+        // register Button
+        add(Box.createRigidArea(new Dimension(0, 5)));
+        JLabel dontHaveAccount074 = new JLabel("Or");
+        dontHaveAccount074.setFont(new Font(Font.SANS_SERIF, Font.PLAIN,18));
+        add(dontHaveAccount074);
+        add(Box.createRigidArea(new Dimension(0, 5)));
+        toRegisterButton074.setFont(new Font(Font.SANS_SERIF, Font.BOLD,25));
+        toRegisterButton074.setText("Register");
+        toRegisterButton074.setForeground(Color.WHITE);
+        toRegisterButton074.setBackground(Color.decode("#2a9d8f"));
+        toRegisterButton074.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
+        toRegisterButton074.setAlignmentX(CENTER_ALIGNMENT);
+        toRegisterButton074.setFocusPainted(false);
+        toRegisterButton074.addActionListener(e->card.show(mainPanel, "register"));
+        add(toRegisterButton074);
+
     }
 }
